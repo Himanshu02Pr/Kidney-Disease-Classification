7d31b948f4d9f2a00eecf6be6dc7ccc752fde098
Himanshu02Pr <himanshu050365@outlook.com>
2025-11-23 05:41:57 +0100

    model training

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from Classifier import logger
 from Classifier.pipeline.Stage01_data_ingestion import DataIngestionPipeline
 from Classifier.pipeline.Stage02_base_model import BaseModelPipeline
+from Classifier.pipeline.Stage03_model_training import ModelTrainingPipeline
 
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-   logger.info(f"<-------- {STAGE_NAME} started -------->") 
+   logger.info(f"\n\n<-------- {STAGE_NAME} started -------->\n\n") 
    data_ingestion = DataIngestionPipeline()
    data_ingestion.main()
-   logger.info(f"<-------- {STAGE_NAME} completed -------->")
+   logger.info(f"\n\n<-------- {STAGE_NAME} completed -------->\n\n")
 except Exception as e:
         logger.exception(e)
         raise e
@@ -17,10 +18,21 @@ except Exception as e:
 
 STAGE_NAME = "Prepare base model"
 try: 
-   logger.info(f"<-------- {STAGE_NAME} started -------->") 
+   logger.info(f"\n\n<-------- {STAGE_NAME} started -------->\n\n") 
    prepare_base_model = BaseModelPipeline()
    prepare_base_model.main()
-   logger.info(f"<-------- {STAGE_NAME} completed -------->")
+   logger.info(f"\n\n<-------- {STAGE_NAME} completed -------->\n\n")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"\n\n<-------- {STAGE_NAME} started -------->\n\n")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f"\n\n<-------- {STAGE_NAME} completed -------->\n\n")
 except Exception as e:
         logger.exception(e)
         raise e
