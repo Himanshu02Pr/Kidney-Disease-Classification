d6c843167e99ed029107c78a70233d7c47b351f6
Himanshu02Pr <himanshu050365@outlook.com>
2025-11-23 05:39:15 +0100

    model training

--- src/Classifier/config/configuration.py
+++ src/Classifier/config/configuration.py
@@ -1,6 +1,9 @@
+import os
 from Classifier.constants import *
 from Classifier.utils.common import read_yaml, create_directories
-from Classifier.entity.config_entity import DataIngestionConfig, BaseModelConfig
+from Classifier.entity.config_entity import (DataIngestionConfig, 
+                                             BaseModelConfig,
+                                             TrainingConfig)
 
 
 class ConfigurationManager:
@@ -43,3 +46,23 @@ class ConfigurationManager:
         )
 
         return base_model_config
+    
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.base_model
+        training_data = os.path.join(self.config.data_ingestion.download_path, "datasets\\nazmul0087\ct-kidney-dataset-normal-cyst-tumor-and-stone\\versions\\1\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=self.params.EPOCHS,
+            params_batch_size=self.params.BATCH_SIZE,
+            params_is_augmentation=self.params.AUGMENTATION,
+            params_image_size=self.params.IMAGE_SIZE
+        )
+
+        return training_config
