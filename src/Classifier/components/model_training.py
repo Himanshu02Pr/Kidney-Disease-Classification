6648e0b4f401b26db1ce4a71427b1a875446f5b0
Himanshu02Pr <himanshu050365@outlook.com>
2025-11-25 05:57:37 +0100

    data flow & augmentation update

--- src/Classifier/components/model_training.py
+++ src/Classifier/components/model_training.py
@@ -1,3 +1,6 @@
+from glob import glob
+import math
+import os
 import tensorflow as tf
 from pathlib import Path
 from Classifier.entity.config_entity import TrainingConfig
@@ -13,61 +16,79 @@ class Training:
             self.config.updated_base_model_path
         )
 
-    def train_valid_generator(self):
-
-        datagenerator_kwargs = dict(
-            rescale = 1./255,
-            validation_split=0.20
-        )
-
-        dataflow_kwargs = dict(
-            target_size=self.config.params_image_size[:-1],
-            batch_size=self.config.params_batch_size,
-            interpolation="bilinear"
-        )
-
-        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
-            **datagenerator_kwargs
-        )
-
-        self.valid_generator = valid_datagenerator.flow_from_directory(
-            directory=self.config.training_data,
-            subset="validation",
-            shuffle=False,
-            **dataflow_kwargs
-        )
-
-        if self.config.params_is_augmentation:
-            train_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
-                rotation_range=40,
-                horizontal_flip=True,
-                width_shift_range=0.2,
-                height_shift_range=0.2,
-                shear_range=0.2,
-                zoom_range=0.2,
-                **datagenerator_kwargs
-            )
-        else:
-            train_datagenerator = valid_datagenerator
-
-        self.train_generator = train_datagenerator.flow_from_directory(
-            directory=self.config.training_data,
-            subset="training",
-            shuffle=True,
-            **dataflow_kwargs
-        )
     
+    def load_data(self):
+
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            self.config.training_data,
+            labels='inferred',
+            label_mode='categorical',
+            validation_split=0.2,
+            subset='training',
+            image_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            seed=42
+        )
+
+        val_ds = tf.keras.utils.image_dataset_from_directory(
+            self.config.training_data,
+            labels='inferred',
+            label_mode='categorical',
+            validation_split=0.2,
+            subset='validation',
+            image_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            seed=42
+        )
+
+        return train_ds, val_ds
+    
+    def sample_count(self):
+        total_count = 0
+        for ext in ['jpg', 'jpeg', 'png', 'bmp']:
+            total_count += len(glob(os.path.join(self.config.training_data, '*', f'*.{ext}')))
+        return total_count
+
+    
+
+    def image_aug(self):
+
+        augment_pipeline = tf.keras.Sequential([
+            tf.keras.layers.RandomFlip("horizontal"),
+            tf.keras.layers.RandomRotation(0.2),
+            tf.keras.layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+        ])
+        return augment_pipeline
       
     def train(self):
-        self.steps_per_epoch = self.train_generator.samples // self.train_generator.batch_size
-        self.validation_steps = self.valid_generator.samples // self.valid_generator.batch_size
+        train_samples = math.floor(self.sample_count()*0.8)
+        val_samples = self.sample_count()-train_samples
+        self.steps_per_epoch = train_samples // self.config.params_batch_size
+        self.validation_steps = val_samples // self.config.params_batch_size
+
+        AUTOTUNE = tf.data.AUTOTUNE
+        augment = self.image_aug()
+
+        train_ds, val_ds = self.load_data()
+
+        if self.config.params_is_augmentation:
+            train_ds = train_ds.map(
+                lambda x, y : (augment(x, training=True),y),
+                num_parallel_calls = AUTOTUNE
+            )
+        else:
+            train_ds
+
+        train_data = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_data = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
 
         self.model.fit(
-            self.train_generator,
+            train_data,
             epochs=self.config.params_epochs,
             steps_per_epoch=self.steps_per_epoch,
             validation_steps=self.validation_steps,
-            validation_data=self.valid_generator
+            validation_data=val_data
         )
 
         self.save_model(
