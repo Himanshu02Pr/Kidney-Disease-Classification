308ed3a7ac9a2071205b7ddb1b1990b5ab92a647
Himanshu02Pr <himanshu050365@outlook.com>
2025-11-26 23:27:15 +0100

    evaluation config added

--- src/Classifier/config/configuration.py
+++ src/Classifier/config/configuration.py
@@ -1,9 +1,9 @@
-import os
 from Classifier.constants import *
 from Classifier.utils.common import read_yaml, create_directories
 from Classifier.entity.config_entity import (DataIngestionConfig, 
                                              BaseModelConfig,
-                                             TrainingConfig)
+                                             TrainingConfig,
+                                             EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -52,14 +52,14 @@ class ConfigurationManager:
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
         prepare_base_model = self.config.base_model
-        training_data = os.path.join(self.config.data_ingestion.download_path, "datasets\\nazmul0087\ct-kidney-dataset-normal-cyst-tumor-and-stone\\versions\\1\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone")
+        #training_data = os.path.join(self.config.data_ingestion.download_path, "datasets\\nazmul0087\ct-kidney-dataset-normal-cyst-tumor-and-stone\\versions\\1\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone\CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone")
         create_directories([Path(training.root_dir)])
 
         training_config = TrainingConfig(
             root_dir=Path(training.root_dir),
             trained_model_path=Path(training.trained_model_path),
             updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
-            training_data=Path(training_data),
+            training_data=Path(training.training_data),
             params_epochs=self.params.EPOCHS,
             params_batch_size=self.params.BATCH_SIZE,
             params_is_augmentation=self.params.AUGMENTATION,
@@ -67,3 +67,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.keras",
+            data_path = Path(self.config.training.training_data),
+            mlflow_uri="https://dagshub.com/Himanshu02Pr/Kidney-Disease-Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
